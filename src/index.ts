9f20f9790d7088bd616301cfac89257e4465bf37
Shums Kassam <skassam21@gmail.com>
2022-08-12 11:54:16 -0400

    [hatchways-commit] - change src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { PORT } from "../env";
 import app from "./app";
 import watch from "../watchfile";
 
+console.log("Another change here");
+
 app.listen(PORT, () => {
   console.log(`Listening: http://localhost:${PORT}`);
 });
